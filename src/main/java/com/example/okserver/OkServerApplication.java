2ada7b77315bd9f2cc6d3873f37ee383a79c068a
kwonssshyeon <kwonsh4396@gmail.com>
2023-07-12 22:21:04 +0900

    initial commit

---
+++ src/main/java/com/example/okserver/OkServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.okserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/okserver/OkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.okserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
